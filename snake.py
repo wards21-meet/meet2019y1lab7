457bafc8619aafa4f70bd8c34ee913e77c488282
wards21-meet <wards21@meet.mit.edu>
2019-07-29 09:59:55 -0400

    Stopping here

---
+++ snake.py
@@ -0,0 +1,65 @@
+import turtle
+import random #We'll need this later in the lab
+
+turtle.tracer(1,0) #This helps the turtle move more smoothly
+
+SIZE_X=800
+SIZE_Y=500
+turtle.setup(SIZE_X, SIZE_Y) #Curious? It's the turtle window  
+                             #size.    
+turtle.penup()
+
+SQUARE_SIZE = 20
+START_LENGTH = 10
+TIME_STEP = 100
+
+#Initialize lists
+pos_list = []
+stamp_list = []
+food_pos = []
+food_stamps = []
+
+#Set up positions (x,y) of boxes that make up the snake
+snake = turtle.clone()
+snake.shape("square")
+
+#Hide the turtle object (it's an arrow - we don't need to see it)
+turtle.hideturtle()
+
+
+#Function to draw a part of the snake on the screen
+
+def new_stamp():
+    snake_pos = snake.pos() #Get snake’s position
+    #Append the position tuple to pos_list
+    pos_list.append(snake_pos) 
+    #snake.stamp() returns a stamp ID. Save it in some variable         
+    gg = snake.stamp()
+    #append that stamp ID to stamp_list.     
+    stamp_list.append(gg)
+
+
+
+
+
+#Draw a snake at the start of the game with a for loop
+#for loop should use range() and count up to the number ofces
+#in the snake (i.e. START_LENGTH)
+for i in range(START_LENGTH):
+    x_pos=snake.pos()[0] #Get x-position with snake.pos()[0]
+    y_pos=snake.pos()[1]
+
+    #Add SQUARE_SIZE to x_pos. Where does x_pos point to now?    
+    # You're RIGHT!
+    x_pos+=SQUARE_SIZE
+
+    snake.goto(x_pos,y_pos) #Move snake to new (x,y)
+   
+    #Now draw the new snake part on the screen (hint, you have a 
+    #function to do this
+    new_stamp()
+
+turtle.mainloop()
+
+
+
